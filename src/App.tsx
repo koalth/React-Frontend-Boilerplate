addecd0eeb6b3a1b113c6f8aa21c1aa468742869
koalth <koalrube@gmail.com>
2024-09-29 16:03:52 -0400

    added more stuff and learning tialwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-
+import Navbar from "./components/Navbar";
+import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <div className="container mx-auto">
-      <h1>Hello World!</h1>
+    <div className="max-w flex flex-col mx-auto size-full">
+      <header className="mb-auto flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4">
+        <Navbar />
+      </header>
+      <main id="content">
+        <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+          <h1 className="block text-2xl font-bold text-black sm:text-4xl">
+            Cover Page
+          </h1>
+          <p className="mt-3 text-lg text-gray-300">
+            A cool little description
+          </p>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+export default function Navbar() {
+  return (
+    <nav className="w-1/2 md:w-full px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between">
+        <a
+          className="flex-none text-xl font-semibold text-black focus:outline-none focus:opacity-80"
+          href="#"
+          aria-label="Brand"
+        >
+          Brand
+        </a>
+        <div className="sm:hidden">
+          <button
+            type="button"
+            className="hs-collapse-toggle relative size-9 flex justify-center items-center border border-black/10 font-medium text-sm text-gray-200 rounded-lg hover:bg-black/10 focus:outline-none focus:bg-black/10"
+            id="hs-navbar-cover-page-collapse"
+            aria-expanded="false"
+            aria-controls="hs-navbar-cover-page"
+            aria-label="Toggle navigation"
+            data-hs-collapse="#hs-navbar-cover-page"
+          >
+            <svg
+              className="hs-collapse-open:hidden shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1={3} x2={21} y1={6} y2={6} />
+              <line x1={3} x2={21} y1={12} y2={12} />
+              <line x1={3} x2={21} y1={18} y2={18} />
+            </svg>
+            <svg
+              className="hs-collapse-open:block hidden shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+            <span className="sr-only">Toggle navigation</span>
+          </button>
+        </div>
+      </div>
+      <div
+        id="hs-navbar-cover-page"
+        className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+        aria-labelledby="hs-navbar-cover-page-collapse"
+      >
+        <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+          <a
+            className="font-medium text-black focus:outline-none"
+            href="#"
+            aria-current="page"
+          >
+            Landing
+          </a>
+          <a
+            className="font-medium text-black/70 hover:text-black focus:outline-none focus:text-black"
+            href="#"
+          >
+            Account
+          </a>
+          <a
+            className="font-medium text-black/70 hover:text-black focus:outline-none focus:text-black"
+            href="#"
+          >
+            Work
+          </a>
+          <a
+            className="font-medium text-black/70 hover:text-black focus:outline-none focus:text-black"
+            href="#"
+          >
+            Blog
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
